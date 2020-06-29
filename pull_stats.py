621bdad67944f93fede68918d85add28595c686f
Kristian Klausen <kristian@klausen.dk>
2020-06-29 23:24:34 +0200

    initial commit

---
+++ pull_stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import json
+import shutil
+
+if subprocess.call(["wget", "--execute", "robots=off", "--recursive", "--no-host-directories", "--no-parent", "--accept", "*.json", "https://flathub.org/stats/"]) != 0:
+    exit(1)
+
+files = []
+refs = {}
+
+for (dirpath, _, filenames) in os.walk("stats"):
+    files.extend(os.path.join(dirpath, filename) for filename in filenames)
+files.sort()
+
+for f in files:
+    print(f)
+    with open(f) as json_file:
+        data = json.load(json_file)
+        date = data["date"]
+        for ref in data["refs"]:
+            refs.setdefault(ref, {"ref": ref, "stats": []})["stats"].append({"date": date, "arches": data["refs"][ref]})
+
+
+os.chdir("web")
+if os.path.isdir("data"):
+    shutil.rmtree("data")
+os.mkdir("data")
+
+def writeJson(f, data):
+    with open(f, "w") as outfile:
+        json.dump(data, outfile)
+
+for ref in refs:
+    writeJson(f"data/{ref.replace('/', '_')}.json", refs[ref])
+
+writeJson("data/refs.json", list(refs.keys()))
---
+++ web/script.js
@@ -0,0 +1,94 @@
+let chart;
+
+function initChart() {
+	let ctx = document.getElementById("chart").getContext("2d");
+	chart = new Chart(ctx, {
+		// The type of chart we want to create
+		type: "line",
+
+		// Configuration options go here
+		options: {
+			scales: {
+				xAxes: [{
+					type: "time",
+					ticks: {}
+				}]
+			}
+		}
+	});
+}
+
+async function refHandler(event) {
+	let ref = event.target.value;
+	let response = await fetch(`./data/${ref.replace("/", "_")}.json`);
+	let json = await response.json();
+
+
+	let chartColors = [
+		"rgb(255, 99, 132)", // red
+		"rgb(255, 159, 64)", // orange
+		"rgb(255, 205, 86)", // yellow
+		"rgb(75, 192, 192)", // green
+		"rgb(54, 162, 235)", // blue
+		"rgb(153, 102, 255)", // purple
+	];
+
+	let datasets = {};
+	for (let dataPoint of json.stats) {
+		for (let arch of Object.keys(dataPoint.arches)) {
+			if (!(arch in datasets)) {
+				let color = chartColors.pop();
+				datasets[arch] = {
+					label: arch,
+					backgroundColor: color,
+					borderColor: color,
+					data: []
+				};
+			}
+			let dataset = datasets[arch];
+			// TODO: Delta downloads
+			// https://github.com/flathub/flathub/issues/177#issuecomment-650122279
+			dataset.data.push({
+				x: new Date(dataPoint.date),
+				y: dataPoint.arches[arch][0]
+			});
+		}
+	}
+	chart.data.datasets = Object.values(datasets);
+	chart.update();
+}
+
+function intervalHandler() {
+	let interval = event.target.value;
+	if (interval === "infinity") {
+		delete chart.options.scales.xAxes[0].ticks.min;
+	} else {
+		let d = new Date();
+		d.setDate(d.getDate() - interval);
+		chart.options.scales.xAxes[0].ticks.min = d;
+	}
+	chart.update();
+}
+
+async function init() {
+	initChart();
+
+	let response = await fetch("./data/refs.json");
+	let json = await response.json()
+	let refSelectElement = document.getElementById("ref-select");
+
+	for (let ref of json) {
+		let option = document.createElement("option");
+		option.text = ref;
+		refSelectElement.add(option);
+	}
+
+	refSelectElement.addEventListener("change", refHandler);
+	refSelectElement.dispatchEvent(new Event("change"));
+
+
+	let intervalSelectElement = document.getElementById("interval-select");
+	intervalSelectElement.addEventListener("change", intervalHandler);
+}
+
+window.addEventListener("DOMContentLoaded", init);
