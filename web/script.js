klausenbusk/flathub-stats

commit 959d1349d672ed3b3d6f0c3e8bf069a6ae328905

Fix incorrect average calculation
Using the number of data points gives the wrong result, so let's
calculate the number of days and use that instead.

Fix #4

web/script.js
@@ -28,17 +28,19 @@ function initChart() {
 
 function updateBasicStats() {
 	let total = 0;
-	let n = 0;
 	let average = 0;
+	let first = min;
 	chart.data.datasets.forEach((dataset) => {
 		dataset.data.forEach((dataPoint) => {
+			if (!min && (!first || dataPoint.y < first)) {
+				first = dataPoint.y;
+			}
 			if (!min || min <= dataPoint.x) {
 				total += dataPoint.y;
-				n++;
 			}
 		})
 	});
-	average = total / n;
+	average = total / Math.round(((new Date()) - min) / (24*60*60*1000));
 	document.getElementById("basic-stats").textContent = `Total: ${total} downloads | Average: ${average.toFixed(2)} downloads per day`;
 }
 

commit 662e34f157eeec60dfd076f77c120473831d1075

Calculate the number of days from the first data point
This fix a issue, where the average is calculated incorrectly, when
the first data point is after the chosen interval. Ex: When you choose
"365 days", but the first data point is 180 days old.
The implementation was started in 959d134, but it wasn't tested or
implemented correctly.

Fixes: 959d134 ("Fix incorrect average calculation")

Fix #4

web/script.js
@@ -29,18 +29,18 @@ function initChart() {
 function updateBasicStats() {
 	let total = 0;
 	let average = 0;
-	let first = min;
+	let first = null;
 	chart.data.datasets.forEach((dataset) => {
 		dataset.data.forEach((dataPoint) => {
-			if (!min && (!first || dataPoint.y < first)) {
-				first = dataPoint.y;
-			}
 			if (!min || min <= dataPoint.x) {
 				total += dataPoint.y;
+				if (!first || dataPoint.x < first) {
+					first = dataPoint.x;
+				}
 			}
 		})
 	});
-	average = total / Math.round(((new Date()) - min) / (24*60*60*1000));
+	average = total / Math.round(((new Date()) - first) / (24*60*60*1000));
 	document.getElementById("basic-stats").textContent = `Total: ${total} downloads | Average: ${average.toFixed(2)} downloads per day`;
 }
 